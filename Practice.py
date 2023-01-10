c4671c9f9f4580487e0634325a8fc7eccff5f503
Akanksha Nemade <akankshakolte817@gmail.com>
2023-01-10 19:03:09 +0530

    If statement

---
+++ Practice.py
@@ -0,0 +1,8 @@
+name = "akhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh"
+if len(name)<3:
+    print("name must be atleast three characters")
+
+elif len(name)>50:
+    print("name should be of maximum 50 characters")
+
+else :print("Name Looks Good!!")
