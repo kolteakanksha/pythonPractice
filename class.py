ccfeff799f8ad3e1c09f5513db0ad6ad57935191
Akanksha Nemade <akankshakolte817@gmail.com>
2023-01-15 14:51:15 +0530

    class

--- class.py
+++ class.py
@@ -3,4 +3,10 @@ class point:
         print("Move the point")
 
     def draw(self):
-        print("")
+        print("draw the point")
+
+
+point1 = point()
+point1.x=10
+point1.move()
+print(point1.x)
