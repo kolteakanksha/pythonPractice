0146d896651a94cd183f65ef39f938c6b40cbb7c
Akanksha Nemade <akankshakolte817@gmail.com>
2023-01-21 20:28:31 +0530

    Inheritance Concept

---
+++ OOPS/Emp.py
@@ -0,0 +1,16 @@
+from OOPS.Person import Person
+
+
+class Emp(Person):
+
+    def Print(self):
+        print("Emp class called")
+
+
+Emp_details = Emp("Mayank", 103)
+
+# calling parent class function
+Emp_details.Display()
+
+# Calling child class function
+Emp_details.Print()
