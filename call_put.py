cf3a5aa8fba8c5c2ee49a314d99acb7138ecf26c
Akanksha Nemade <akankshakolte817@gmail.com>
2023-02-18 22:53:02 +0530

    put method in api

---
+++ call_put.py
@@ -0,0 +1,10 @@
+import requests
+
+id= input("what is the id you want to update? ")
+name= input("What is the new name? ")
+employee={"name": name}
+headers={"Content-type": "application/json"}
+resp = requests.post(f"https://reqres.in/api/users{id}", json=employee, headers=headers )
+info= resp.json()
+print(info)
+print(resp.status_code)
