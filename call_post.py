3d8513a80bdf732ed0a6c84aef463ac4d4b7ba7f
Akanksha Nemade <akankshakolte817@gmail.com>
2023-02-18 22:50:12 +0530

    post method in api

--- call_post.py
+++ call_post.py
@@ -1,2 +1,9 @@
 import requests
 
+name= input("What is the new name? ")
+employee={"name": name}
+headers={"Content-type": "application/json"}
+resp = requests.post(f"https://reqres.in/api/users", json=employee, headers=headers )
+info= resp.json()
+print(info)
+print(resp.status_code)
